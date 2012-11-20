40a7751f579336df5e8a8ba3a53525850dd11491
Senai Mesfin <yonata09@gmail.com>
2012-11-19 22:55:53 -0600

    Resolved merge conflicts

--- UltimateScheduler/gen/com/mistypanda/ultimatescheduler/R.java
+++ UltimateScheduler/gen/com/mistypanda/ultimatescheduler/R.java
@@ -15,16 +15,38 @@ public final class R {
         public static final int white=0x7f040000;
     }
     public static final class drawable {
-        public static final int header_background=0x7f020002;
+        public static final int header_background=0x7f020004;
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int shape=0x7f020002;
+        public static final int shape2=0x7f020003;
     }
     public static final class id {
+        public static final int LinearLayout1=0x7f080017;
+        public static final int ListView1=0x7f080010;
+        public static final int RelativeLayout1=0x7f080005;
+        public static final int TextView01=0x7f08000d;
+        public static final int TextView02=0x7f08000b;
+        public static final int TextView03=0x7f080009;
+        public static final int background=0x7f080016;
+        public static final int button1=0x7f08000f;
+        public static final int date=0x7f080014;
+        public static final int eventname=0x7f080011;
+        public static final int frameLayersLayout=0x7f080015;
         public static final int gridview=0x7f080004;
         public static final int header=0x7f080000;
-        public static final int menu_settings=0x7f080005;
+        public static final int label=0x7f080012;
+        public static final int location=0x7f080013;
+        public static final int menu_settings=0x7f080019;
+        public static final int newEventDate=0x7f08000c;
+        public static final int newEventDetails=0x7f08000e;
+        public static final int newEventLocation=0x7f08000a;
+        public static final int newEventName=0x7f080008;
         public static final int next=0x7f080003;
         public static final int previous=0x7f080001;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f080007;
+        public static final int textView3=0x7f080018;
         public static final int title=0x7f080002;
     }
     public static final class layout {
@@ -32,6 +54,11 @@ public final class R {
         public static final int activity_create_event=0x7f030001;
         public static final int activity_events=0x7f030002;
         public static final int activity_home=0x7f030003;
+        public static final int createactivity=0x7f030004;
+        public static final int eventlist=0x7f030005;
+        public static final int eventlistview=0x7f030006;
+        public static final int homepage=0x7f030007;
+        public static final int viewevent=0x7f030008;
     }
     public static final class menu {
         public static final int activity_home=0x7f070000;

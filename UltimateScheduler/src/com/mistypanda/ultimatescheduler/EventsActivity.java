079c677643b43fc3d546d41f48b9853227d3f924
Senai Mesfin <yonata09@gmail.com>
2012-11-19 22:16:41 -0600

    Resolved merge conflicts

--- UltimateScheduler/src/com/mistypanda/ultimatescheduler/EventsActivity.java
+++ UltimateScheduler/src/com/mistypanda/ultimatescheduler/EventsActivity.java
@@ -1,16 +1,47 @@
 package com.mistypanda.ultimatescheduler;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class EventsActivity extends Activity {
 
+public class EventsActivity extends ListActivity {
+	Event [] events = new Event[3];
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	
+	    
+	    
+	    
+	    for (int i=0;i<3;i++){
+	    	events[i] = new Event("Eventss " + i, "location " + i , i + "/10/1020");
+	    }
+
+	    EventListAdapter eventAdapter = new EventListAdapter(this,events);
+	    setListAdapter(eventAdapter);
 	    // TODO Auto-generated method stub
 	}
 
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+ 
+		//get selected items
+		Event selectedEvent= events[position];
+		Intent viewEventIntent = new Intent(this, EventActivity.class);
+		//add event to intent.
+		//call intent.
+		//how to retrieve event from intent?
+		String selectedValue = (String) getListAdapter().getItem(position);
+		//Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+ 
+	}
+	
+	
 }

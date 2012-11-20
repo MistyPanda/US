058d5c9fc814beaacf856958d61ca4d50576b8ab
Senai Mesfin <yonata09@gmail.com>
2012-11-19 22:19:28 -0600

    Resolved merge conflicts

--- UltimateScheduler/gen/com/mistypanda/ultimatescheduler/R.java
+++ UltimateScheduler/gen/com/mistypanda/ultimatescheduler/R.java
@@ -22,12 +22,14 @@ public final class R {
         public static final int shape2=0x7f020003;
     }
     public static final class id {
+<<<<<<< HEAD
         public static final int gridview=0x7f080004;
         public static final int header=0x7f080000;
         public static final int menu_settings=0x7f080005;
         public static final int next=0x7f080003;
         public static final int previous=0x7f080001;
         public static final int title=0x7f080002;
+=======
         public static final int LinearLayout1=0x7f070013;
         public static final int ListView1=0x7f07000c;
         public static final int RelativeLayout1=0x7f070001;
@@ -50,22 +52,27 @@ public final class R {
         public static final int textView1=0x7f070002;
         public static final int textView2=0x7f070003;
         public static final int textView3=0x7f070014;
+>>>>>>> 32348e3620e92b7f45325891fb7b19852bdb15c7
     }
     public static final class layout {
         public static final int activity_calendar=0x7f030000;
         public static final int activity_create_event=0x7f030001;
         public static final int activity_events=0x7f030002;
         public static final int activity_home=0x7f030003;
+<<<<<<< HEAD
+=======
         public static final int createactivity=0x7f030004;
         public static final int eventlist=0x7f030005;
         public static final int eventlistview=0x7f030006;
         public static final int homepage=0x7f030007;
         public static final int viewevent=0x7f030008;
+>>>>>>> 32348e3620e92b7f45325891fb7b19852bdb15c7
     }
     public static final class menu {
         public static final int activity_home=0x7f070000;
     }
     public static final class string {
+<<<<<<< HEAD
         public static final int app_name=0x7f050000;
         public static final int get_all_event=0x7f050009;
         public static final int get_event=0x7f050008;
@@ -76,6 +83,7 @@ public final class R {
         public static final int title_section1=0x7f050003;
         public static final int title_section2=0x7f050002;
         public static final int title_section3=0x7f050001;
+=======
         public static final int app_name=0x7f040000;
         public static final int copyright=0x7f04000a;
         public static final int hello_world=0x7f040004;
@@ -87,6 +95,7 @@ public final class R {
         public static final int title_section1=0x7f040003;
         public static final int title_section2=0x7f040002;
         public static final int title_section3=0x7f040001;
+>>>>>>> 32348e3620e92b7f45325891fb7b19852bdb15c7
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;

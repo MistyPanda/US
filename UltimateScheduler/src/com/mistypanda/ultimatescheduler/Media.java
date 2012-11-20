0fe034f7472a52364dabfa989aac8b354665b994
Matthew Scherber <mcscherber@csbsju.edu>
2012-11-19 23:33:09 -0600

    added media.java file

--- UltimateScheduler/src/com/mistypanda/ultimatescheduler/Media.java
+++ UltimateScheduler/src/com/mistypanda/ultimatescheduler/Media.java
@@ -9,4 +9,23 @@ public class Media {
 
 	//MediaID
 	//EventID
+	
+	String mediaID, eventID;
+	
+	public String getMediaID(){
+		return this.mediaID;
+	}
+	
+	public String getEventID(){
+		return this.eventID;
+	}
+	
+	public void setMediaID(String mediaID){
+		this.mediaID = mediaID;
+	}
+	
+	public void setEventID(String eventID){
+		this.eventID = eventID;
+	}
+	
 }

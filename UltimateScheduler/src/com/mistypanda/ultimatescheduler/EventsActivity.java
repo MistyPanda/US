285c96c2761c7241f197d7cbc6aae58585338b89
Senai Mesfin <yonata09@gmail.com>
2012-11-20 03:20:02 -0600

    tired of this shit...

--- UltimateScheduler/src/com/mistypanda/ultimatescheduler/EventsActivity.java
+++ UltimateScheduler/src/com/mistypanda/ultimatescheduler/EventsActivity.java
@@ -1,63 +1,86 @@
 package com.mistypanda.ultimatescheduler;
 
+import java.util.ArrayList;
+
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 import com.mistypanda.ultimatescheduler.DBAccess.*;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
-
-public class EventsActivity extends ListActivity {
 	List<Event> events; 
+public class EventsActivity extends Activity// extends ListActivity 
+{
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    /*
-	    try {
-			setUpEvents();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    
-	    */
-	    try{
+
 	   //events = (Event[])DBHelper.getAllEvents().toArray();
+		setContentView(R.layout.eventlist);
+	    LayoutInflater inflater = getLayoutInflater();
+	    //LinearLayout layout = (LinearLayout)inflater.inflate(R.layout.eventlist,null);
+	    ListView listView = (ListView)findViewById(R.id.eventlistviewer);
 	   
-	   events = DBHelper.getAllEvents();
-	    }
-	    catch(Exception e){
-	    	System.out.println(e.getMessage());	
-	    }
-	   
-	    System.out.println(events.get(1).getEventName());
+	    try{
+	 	   events = DBHelper.getAllEvents();
+	 	    	//events.add(DBHelper.getEventByEventID(1));
+	 	    }	   
+
+
+
+
+
+	 	    catch(Exception e){
+	 	    	System.out.println(e.getMessage());	
+	 	    }
+	    
+	    //System.out.println(events.get(0).getLocation());
 	   EventListAdapter eventAdapter = new EventListAdapter(this,events);
-	    setListAdapter(eventAdapter);
+	    listView.setAdapter(eventAdapter);
+	    final Intent viewEventIntent = new Intent(this, EventDetailsActivity.class);
+	   listView.setOnItemClickListener(new OnItemClickListener() {
+	    	  public void onItemClick(AdapterView<?> parent, View view,
+	    	    int position, long id) {
+	    		  Event selectedEvent= events.get(position);
+	    			
+	    			viewEventIntent.putExtra("Event", selectedEvent);
+	    			startActivity(viewEventIntent);
+	    	  }
+	    	}); 
 	    // TODO Auto-generated method stub
 	     
 	    
 	}
 	
 	@Override
+	//@Override
+	public void addEventClick(View view){
+		Intent intent = new Intent(this, CreateEventActivity.class);
+		startActivity(intent);
+		
+	}
+	
+	
 	protected void onListItemClick(ListView l, View v, int position, long id) {
  
 		//get selected items
 		Event selectedEvent= events.get(position);
 		//Intent viewEventIntent = new Intent(this, EventActivity.class);
+		//viewEventIntent.putExtra("Event", selectedEvent);
+		//startActivity(viewEventIntent);
 		//add event to intent.
 		//call intent.
 		//how to retrieve event from intent?

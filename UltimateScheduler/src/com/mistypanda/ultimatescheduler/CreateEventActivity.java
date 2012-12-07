53a806363166c8f2da0e3d46f065b70c5d397ac5
Matthew Scherber <mcscherber@csbsju.edu>
2012-12-07 02:42:13 -0600

    I changed a lot of the GUI, homepage, createevent, eventlist, evenetdetails, datepicker, time picker, revamed all layouts:  fonts, buttons, textfields, edittexts...

--- UltimateScheduler/src/com/mistypanda/ultimatescheduler/CreateEventActivity.java
+++ UltimateScheduler/src/com/mistypanda/ultimatescheduler/CreateEventActivity.java
@@ -1,43 +1,144 @@
 package com.mistypanda.ultimatescheduler;
 
+import java.util.Calendar;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.mistypanda.ultimatescheduler.DBAccess.*;
 
 public class CreateEventActivity extends Activity {
 
+	private int mYear, secYear;
+	private int mMonth, secMonth;
+	private int mDay, secDay;
+
+	private TextView mDateDisplay;
+	private Button mPickDate;
+	private TextView secondDateDisplay;
+	private Button secondPickDate;
+	
+	private TextView firstTimeDisplay;
+	private Button firstPickTime;
+	private TextView secondTimeDisplay;
+	private Button secondPickTime;
+
+	static final int DATE_DIALOG_ID = 0;
+	static final int DATE_DIALOG_ID2 = 0;
+	
+	
+	
+ 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+	
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.createactivity);
-	    // TODO Auto-generated method stub
+	 
+	    mDateDisplay = (TextView) findViewById(R.id.showStartDate);        
+	    mPickDate = (Button) findViewById(R.id.myStartDateButton);
+		
+		
+	    secondDateDisplay = (TextView) findViewById(R.id.showEndDate);        
+	    secondPickDate = (Button) findViewById(R.id.myEndDateButton);
+	    
+
+	    mPickDate.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	            showDialog(DATE_DIALOG_ID);
+	        }
+	    });
+	    
+	    secondPickDate.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	            showDialog(DATE_DIALOG_ID2);
+	        }
+	    });
+
+	    // get the current date
+	    final Calendar c = Calendar.getInstance();
+	    mYear = c.get(Calendar.YEAR);
+	    mMonth = c.get(Calendar.MONTH);
+	    mDay = c.get(Calendar.DAY_OF_MONTH);
+	    	    
+	    updateDisplay();
+  
 	}
+	
+    // display the current date
+    private void updateDisplay() {
+    	this.secondDateDisplay.setText(new StringBuilder()
+        	// Month is 0 based so add 1
+        	.append(secMonth + 1).append("-")
+        	.append(secDay).append("-")
+        	.append(secYear).append(" "));
+    	
+        this.mDateDisplay.setText(
+            new StringBuilder()
+                    // Month is 0 based so add 1
+                    .append(mMonth + 1).append("-")
+                    .append(mDay).append("-")
+                    .append(mYear).append(" "));
+    }
+    
+    
+	
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+    	    new DatePickerDialog.OnDateSetListener() {
+    	        public void onDateSet(DatePicker view, int year, 
+    	                              int monthOfYear, int dayOfMonth) {
+    	            mYear = year;
+    	            mMonth = monthOfYear;
+    	            mDay = dayOfMonth;
+    	            secYear = year;
+    	            secMonth = monthOfYear;
+    	            secDay = dayOfMonth;
+    	            updateDisplay();
+    	        }
+    	    };
+    	    
+    	   
+    @Override
+    protected Dialog onCreateDialog(int id) {
+       switch (id) {
+       case DATE_DIALOG_ID:
+          return new DatePickerDialog(this,
+                    mDateSetListener,
+                    mYear, mMonth, mDay);
+       }
+       return null;
+    }  
+	
+
 	
 	public void createClick(View view){
 		LayoutInflater inflater = getLayoutInflater();
-		EditText name = (EditText)findViewById(R.id.createEventName);
-		EditText startDate = (EditText)findViewById(R.id.createEventStartDate);
-		EditText endDate = (EditText)findViewById(R.id.createEventEndDate);
-		EditText host = (EditText)findViewById(R.id.createEventHost);
-		EditText location = (EditText)findViewById(R.id.createEventLocation);
-		EditText info = (EditText)findViewById(R.id.createEventInfo);
+		EditText name = (EditText)findViewById(R.id.eventNameET);
+		//DatePicker startDate = (DatePicker)findViewById(R.id.startDateTV);
+		//DatePicker endDate = (DatePicker)findViewById(R.id.endDateTV);
+		EditText host = (EditText)findViewById(R.id.eventHostET);
+		EditText location = (EditText)findViewById(R.id.locationET);
+		EditText description = (EditText)findViewById(R.id.descriptionET);
 		
 		String eNam = name.getText().toString();
 		String eLoc = location.getText().toString();
 		String eHost = host.getText().toString();
-		String eStart	= startDate.getText().toString();
-		String eEnd	= endDate.getText().toString();
-		String descrip = info.getText().toString();
+		String eStart = null;	/*= startDate.getText().toString();*/
+		String eEnd = null;	/*= endDate.getText().toString();*/
+		String descrip = description.getText().toString();
 		
 		try {
 			DBHelper.addEvent(eNam, eLoc, eHost, eStart, eEnd, descrip);

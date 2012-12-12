14dca996a8829f010cb46e6383f27cf1658b8e5c
Matthew Scherber <mcscherber@csbsju.edu>
2012-12-12 03:36:49 -0600

    Added two files, edit event xml and java file

---
+++ UltimateScheduler/src/com/mistypanda/ultimatescheduler/EditEventActivity.java
@@ -0,0 +1,28 @@
+package com.mistypanda.ultimatescheduler;
+
+import android.app.Activity;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.TimePicker;
+
+import com.mistypanda.ultimatescheduler.DBAccess.*;
+
+public class EditEventActivity extends Activity{
+
+}
